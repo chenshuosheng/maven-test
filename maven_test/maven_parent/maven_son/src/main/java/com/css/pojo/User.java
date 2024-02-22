9a947b3ff275dc7aa263b1e067bd2a892e5f1495
Shuosheng Chen <1363318229@qq.com>
2024-02-22 22:38:56 +0800

    maven继承，关注dependencyManagement

---
+++ maven_test/maven_parent/maven_son/src/main/java/com/css/pojo/User.java
@@ -0,0 +1,17 @@
+package com.css.pojo;
+
+import lombok.Data;
+
+/**
+ * @Description: TODO
+ * @Author: CSS
+ * @Date: 2024/2/22 22:16
+ */
+
+@Data
+public class User {
+
+    private Integer age;
+
+    private String username;
+}
